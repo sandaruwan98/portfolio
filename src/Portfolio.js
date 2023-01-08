6329c826db86ae707b649989a62b042dab9b7493
sandaruwan98 <lakshansandaruwan1998@gmail.com>
2023-01-08 23:49:21 +0530

    restructure

---
+++ src/Portfolio.js
@@ -0,0 +1,12 @@
+// import { Window, TitleBar, SegmentedControl, SegmentedControlItem, Text } from 'react-desktop/macOs'
+
+export default function Portfolio() {
+  return (
+    // <Window chrome width="1067px" height="683px" padding="10px">
+    //   <TitleBar title="untitled text 5" controls />
+
+    // </Window>
+
+    <iframe width="1080px" height="710px" src="portfoliohtml/index.html" />
+  )
+}
