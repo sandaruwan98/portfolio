c594b3c6a7466ac00622c94315825be0c0337afc
sandaruwan98 <lakshansandaruwan1998@gmail.com>
2023-01-10 00:38:12 +0530

    placment code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Environment, PresentationControls } from '@react-three/drei'
 import Laptop from './Laptop'
 
 export default function App() {
-  return (
-    <Canvas camera={{ position: [-5, 0, -15], fov: 55 }}>
-      <pointLight position={[10, 10, 10]} intensity={1.5} />
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 })
+  const [rposition, setrPosition] = useState({ rx: 0, ry: 0, rz: 0 })
+  const { x, y, z } = position
+  const { rx, ry, rz } = rposition
 
-      {/* <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} /> */}
-      <PresentationControls
-        global
-        config={{ mass: 2, tension: 500 }}
-        snap={{ mass: 4, tension: 1500 }}
-        rotation={[0, 0.3, 0]}
-        polar={[-Math.PI / 3, Math.PI / 3]}
-        azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
-        <Suspense fallback={null}>
-          <group rotation={[-0.4, Math.PI * 1.2, 0]} position={[1.3, 0.5, -4]}>
-            <Laptop />
-          </group>
-          <Environment preset="city" />
-        </Suspense>
-      </PresentationControls>
-    </Canvas>
+  return (
+    <>
+      <div className="controls">
+        <label>x</label>
+        <input onChange={(e) => setPosition({ ...position, x: e.target.value })} value={position.x} type="number" />
+        <label>y</label>
+        <input onChange={(e) => setPosition({ ...position, y: e.target.value })} value={position.y} type="number" />
+        <label>z</label>
+        <input onChange={(e) => setPosition({ ...position, z: e.target.value })} value={position.z} type="number" />
+      </div>
+      <div className="controls">
+        <label>x</label>
+        <input onChange={(e) => setrPosition({ ...rposition, rx: e.target.value })} value={rposition.rx} type="number" />
+        <label>y</label>
+        <input onChange={(e) => setrPosition({ ...rposition, ry: e.target.value })} value={rposition.ry} type="number" />
+        <label>z</label>
+        <input onChange={(e) => setrPosition({ ...rposition, rz: e.target.value })} value={rposition.rz} type="number" />
+      </div>
+      <Canvas camera={{ position: [-5, 0, -15], fov: 55 }}>
+        <pointLight position={[10, 10, 10]} intensity={1.5} />
+
+        {/* <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} /> */}
+        <PresentationControls
+          global
+          config={{ mass: 2, tension: 500 }}
+          snap={{ mass: 4, tension: 1500 }}
+          rotation={[0, 0.3, 0]}
+          polar={[-Math.PI / 3, Math.PI / 3]}
+          azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
+          <Suspense fallback={null}>
+            <group rotation={[-0.4, Math.PI * 1.2, 0]} position={[1.3, 0.5, -4]}>
+              <Laptop x={x} y={y} z={z} rx={rx} ry={ry} rz={rz} />
+            </group>
+            <Environment preset="city" />
+          </Suspense>
+        </PresentationControls>
+      </Canvas>
+    </>
   )
 }
--- src/Laptop.js
+++ src/Laptop.js
@@ -1,47 +1,103 @@
-
 import * as THREE from 'three'
-import React, {  useRef } from 'react'
-import {  useFrame } from '@react-three/fiber'
-import { Html, useGLTF} from '@react-three/drei'
+import React, { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { Html, useGLTF } from '@react-three/drei'
 import Portfolio from './Portfolio'
 
-
 export default function Laptop(props) {
-    const group = useRef()
-    // Load model
-    const { nodes, materials } = useGLTF('/mac-draco.glb')
-    // Make it float
-    useFrame((state) => {
-      const t = state.clock.getElapsedTime()
-      group.current.rotation.x = THREE.MathUtils.lerp(group.current.rotation.x, Math.cos(t / 2) / 20 + 0.25, 0.1)
-      group.current.rotation.y = THREE.MathUtils.lerp(group.current.rotation.y, Math.sin(t / 4) / 20, 0.1)
-      group.current.rotation.z = THREE.MathUtils.lerp(group.current.rotation.z, Math.sin(t / 8) / 20, 0.1)
-      group.current.position.y = THREE.MathUtils.lerp(group.current.position.y, (-2 + Math.sin(t / 2)) / 2, 0.1)
-    })
-  
-    
-    return (
-      <group ref={group} {...props} dispose={null}>
-        <group rotation-x={-0.425} position={[0, -0.04, 0.41]}>
-          <group position={[0, 2.96, -0.13]} rotation={[Math.PI / 2, 0, 0]}>
-            <mesh material={materials.aluminium} geometry={nodes['Cube008'].geometry} />
-            <mesh material={materials['matte.001']} geometry={nodes['Cube008_1'].geometry} />
-            <mesh geometry={nodes['Cube008_2'].geometry}>
-              {/* Drei's HTML component can "hide behind" canvas geometry */}
-              <Html className="content" rotation-x={-Math.PI / 2} position={[0, 0.05, -0.09]} scale={0.3} transform occlude>
-                <div className="wrapper" onPointerDown={(e) => e.stopPropagation()}>
-                  <Portfolio />
-                </div>
-              </Html>
-            </mesh>
-          </group>
+  const vec = new THREE.Vector3(props.x, props.y, props.z)
+  const vec1 = new THREE.Vector3(props.rx, props.ry, props.rz)
+
+  // const [zoomed, setZoomed] = useState(false)
+  // const [floating, setFloating] = useState(true)
+  var zoomed = false
+  var floating = true
+  const group = useRef()
+  // Load model
+  const { nodes, materials } = useGLTF('/mac-draco.glb')
+  const initialpos = new THREE.Vector3(0, 0, 0)
+  const initialrotation = new THREE.Vector3(0, 0, 0)
+  // console.log(initialpos);
+  // console.log(initialrotation);
+  const zoompos = new THREE.Vector3(-2.6, -0.55, 6)
+  const zoomrotation = new THREE.Vector3(0.006, -0.6, -0.01)
+  // Make it float
+  useFrame((state) => {
+    group.current.position.lerp(vec, 0.1)
+    group.current.rotation.x = THREE.MathUtils.lerp(group.current.rotation.x, vec1.x, 0.1)
+    group.current.rotation.y = THREE.MathUtils.lerp(group.current.rotation.y, vec1.y, 0.1)
+    group.current.rotation.z = THREE.MathUtils.lerp(group.current.rotation.z, vec1.z, 0.1)
+
+    // const t = state.clock.getElapsedTime()
+    // // console.log(t)
+    // if (floating) {
+    //   group.current.rotation.x = THREE.MathUtils.lerp(group.current.rotation.x, Math.cos(t / 2) / 20 + 0.25, 0.1)
+    //   group.current.rotation.y = THREE.MathUtils.lerp(group.current.rotation.y, Math.sin(t / 4) / 20, 0.1)
+    //   group.current.rotation.z = THREE.MathUtils.lerp(group.current.rotation.z, Math.sin(t / 8) / 20, 0.1)
+    //   group.current.position.y = THREE.MathUtils.lerp(group.current.position.y, (-2 + Math.sin(t / 2)) / 2, 0.1)
+    // } else {
+    //   if (zoomed) {
+    //     group.current.position.x = THREE.MathUtils.lerp(group.current.position.x, zoompos.x, 0.1)
+    //     group.current.position.y = THREE.MathUtils.lerp(group.current.position.y, zoompos.y, 0.1)
+    //     group.current.position.z = THREE.MathUtils.lerp(group.current.position.z, zoompos.z, 0.1)
+
+    //     group.current.rotation.x = THREE.MathUtils.lerp(group.current.rotation.x, zoomrotation.x, 0.1)
+    //     group.current.rotation.y = THREE.MathUtils.lerp(group.current.rotation.y, zoomrotation.y, 0.1)
+    //     group.current.rotation.z = THREE.MathUtils.lerp(group.current.rotation.z, zoomrotation.z, 0.1)
+    //   } else {
+    //     group.current.position.x = THREE.MathUtils.lerp(group.current.position.x, initialpos.x, 0.1)
+    //     group.current.position.y = THREE.MathUtils.lerp(group.current.position.y, initialpos.y, 0.1)
+    //     group.current.position.z = THREE.MathUtils.lerp(group.current.position.z, initialpos.z, 0.1)
+
+    //     group.current.rotation.x = THREE.MathUtils.lerp(group.current.rotation.x, initialrotation.x, 0.1)
+    //     group.current.rotation.y = THREE.MathUtils.lerp(group.current.rotation.y, initialrotation.y, 0.1)
+    //     group.current.rotation.z = THREE.MathUtils.lerp(group.current.rotation.z, initialrotation.z, 0.1)
+
+    //     // group.current.position.lerp(initialpos, 0.1)
+    //     // group.current.rotation.lerp(initialrotation, 0.1)
+    //     // group.current.position = THREE.MathUtils.lerp(group.current.position, initialpos, 0.1)
+    //     // group.current.rotation = THREE.MathUtils.lerp(group.current.rotation, initialrotation, 0.1)
+    //   }
+    // }
+  })
+
+  return (
+    <group
+      onClick={() => {
+        console.log('test')
+        // if (zoomed == false && floating == true) {
+        //   floating = false
+        //   zoomed = true
+        // } else if (zoomed == true) {
+        //   zoomed = false
+        //   setTimeout(() => {
+        //     floating = true
+        //   }, 1500)
+        // }
+      }}
+      ref={group}
+      {...props}
+      dispose={null}>
+      <group rotation-x={-0.425} position={[0, -0.04, 0.41]}>
+        <group position={[0, 2.96, -0.13]} rotation={[Math.PI / 2, 0, 0]}>
+          <mesh material={materials.aluminium} geometry={nodes['Cube008'].geometry} />
+          <mesh material={materials['matte.001']} geometry={nodes['Cube008_1'].geometry} />
+          <mesh geometry={nodes['Cube008_2'].geometry}>
+            {/* Drei's HTML component can "hide behind" canvas geometry */}
+            <Html className="content" rotation-x={-Math.PI / 2} position={[0, 0.05, -0.09]} scale={0.3} transform occlude>
+              <div className="wrapper" onPointerDown={(e) => e.stopPropagation()}>
+                <Portfolio />
+              </div>
+            </Html>
+          </mesh>
         </group>
-        <mesh material={materials.keys} geometry={nodes.keyboard.geometry} position={[1.79, 0, 3.45]} />
-        <group position={[0, -0.1, 3.39]}>
-          <mesh material={materials.aluminium} geometry={nodes['Cube002'].geometry} />
-          <mesh material={materials.trackpad} geometry={nodes['Cube002_1'].geometry} />
-        </group>
-        <mesh material={materials.touchbar} geometry={nodes.touchbar.geometry} position={[0, -0.03, 1.2]} />
       </group>
-    )
-  }
+      <mesh material={materials.keys} geometry={nodes.keyboard.geometry} position={[1.79, 0, 3.45]} />
+      <group position={[0, -0.1, 3.39]}>
+        <mesh material={materials.aluminium} geometry={nodes['Cube002'].geometry} />
+        <mesh material={materials.trackpad} geometry={nodes['Cube002_1'].geometry} />
+      </group>
+      <mesh material={materials.touchbar} geometry={nodes.touchbar.geometry} position={[0, -0.03, 1.2]} />
+    </group>
+  )
+}
